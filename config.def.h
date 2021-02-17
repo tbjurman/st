4bc344cf105093d0b076f0fe964b01ca0d0168ad
Tomas Bjurman <tbjurman@gmail.com>
2021-02-17 15:57:12 +0100

    font: adobe source code pro (with horiz tweek)

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "source code pro:size=10";
 static int borderpx = 2;
 
 /*
@@ -27,7 +28,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.0;
+static float chscale = 0.9;
 
 /*
  * word delimiter string
